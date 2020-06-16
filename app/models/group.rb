a121d112b3e19d2eb525e47ef9357d89e5f881b8
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-06-16 17:28:40 +0900

    to be called in side-bar
    showing latest message in side-bar for each group

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,17 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.text?
+        last_message.text
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
 end
