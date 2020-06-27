c8fccc3d46cccb9b274a356ccfb627dda3fd36f7
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-06-28 02:52:28 +0900

    Function adding users bottom of searching frame

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,11 @@
+$(function(){
+  function addUser(user) {
+    let html = `
+    <div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${user.nickname}</p>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.nickname}">追加</div>
+    </div>
+    `;
+    $("#user-search-result").append(html);
+  }
+
