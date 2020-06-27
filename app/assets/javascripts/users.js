5070b4760fe129536f6d78b1285800c401c0bd5b
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-06-28 03:30:25 +0900

    Action when "追加" is clicked
    User name shown in searching result will be delete and added to group member list

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -58,3 +58,12 @@ $(function(){
       });
   });
 
+  $(document).on('click', '.chat-group-user__btn--add', function() {
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
