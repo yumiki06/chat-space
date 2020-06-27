e92a1d4ea508af21d2188b80d3344f33080bfb43
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-06-28 03:22:17 +0900

    Incremental search for users
    Add ajax. Keyword is from Form's name.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -32,3 +32,11 @@ $(function(){
     $(`#${userId}`).append(html);
   }
 
+  $('#user-search-field').on('keyup', function(){
+    let input = $('#user-search-field').val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
