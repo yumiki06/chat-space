cf850d6b3acdb8596dae4711c90b9e419d142feb
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-06-29 19:33:36 +0900

    Add reloadMessages definition and ajax
    Getting latest message's id

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -66,3 +66,15 @@ $(function(){
       })
   });
 });
+  let reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    let last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
