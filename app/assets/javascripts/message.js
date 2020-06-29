f0695fecdd99395b02d11024ff94bebbad9184b7
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-06-29 19:10:59 +0900

    Add custom data attributes to ".message" div

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-      `<div class="message">
+      `<div class="message" data-message-id=${message.id}>
         <div class="message__upper-info">
           <p class="message__upper-info__name">
             ${message.user_nickname}
@@ -22,7 +22,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="message">
+      `<div class="message" data-message-id=${message.id}>
         <div class="message__upper-info">
           <p class="message__upper-info__name">
             ${message.user_nickname}
