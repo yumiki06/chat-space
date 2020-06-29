4f0e2bd782cd0919b1eb061d4333147e3c1b6049
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-06-29 20:03:01 +0900

    Function showing s new message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -78,3 +78,16 @@ $(function(){
       //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        let insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.main-chat__message-list').append(insertHTML);
+        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      }
+    })
